2f4a2b63d8ffa2dcd0879073cf04bdc040d49eb4
Aditya Raj <138434890+Raj-Aditya-27@users.noreply.github.com>
2024-08-05 01:58:12 +0530

    background colour added

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import "../css/HomePage.css"
-import img1 from '../images/img1.jpg';
+import React from "react";
+import "../css/HomePage.css";
+import img1 from "../images/img1.jpg";
 
 function HomePage() {
   return (
-    <div className='frame'>
-        <div className='left'>Left</div>
-        <div className='right'>
-            <img src={img1} alt="Happy girl with oranges" />
-        </div>
+    <div className="frame">
+      <div className="left">Left</div>
+      <div className="right">
+        <img src={img1} alt="Happy girl with oranges" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
