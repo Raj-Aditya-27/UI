b31b6930a42f7db03cb38648ec1ef2fbf4189497
Aditya Raj <138434890+Raj-Aditya-27@users.noreply.github.com>
2024-08-05 04:00:04 +0530

    icon added

--- src/components/LeftFrame.jsx
+++ src/components/LeftFrame.jsx
@@ -1,17 +1,45 @@
 import React from "react";
+
 import "../css/LeftFrame.css";
 
 function LeftFrame() {
   return (
     <div className="leftFrame">
-      <h3><span className="specialColor">Welcome</span> Back</h3>
+      <h3>
+        <span className="specialColor">Welcome</span> Back
+        <svg
+          className="svg"
+          xmlns="http://www.w3.org/2000/svg"
+          width="1em"
+          height="1em"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="#f23e1e"
+            d="M23 17c0 3.31-2.69 6-6 6v-1.5c2.5 0 4.5-2 4.5-4.5zM1 7c0-3.31 2.69-6 6-6v1.5c-2.5 0-4.5 2-4.5 4.5zm7-2.68l-4.59 4.6c-3.22 3.22-3.22 8.45 0 11.67s8.45 3.22 11.67 0l7.07-7.09c.49-.47.49-1.26 0-1.75a1.25 1.25 0 0 0-1.77 0l-4.42 4.42l-.71-.71l6.54-6.54c.49-.49.49-1.28 0-1.77s-1.29-.49-1.79 0L14.19 13l-.69-.73l6.87-6.89c.49-.49.49-1.28 0-1.77s-1.28-.49-1.77 0l-6.89 6.89l-.71-.7l5.5-5.48c.5-.49.5-1.28 0-1.77s-1.28-.49-1.77 0l-7.62 7.62a4 4 0 0 1-.33 5.28l-.71-.71a3 3 0 0 0 0-4.24l-.35-.35l4.07-4.07c.49-.49.49-1.28 0-1.77c-.5-.48-1.29-.48-1.79.01"
+          />
+        </svg>
+      </h3>
       <h6>Please enter your details</h6>
       <div className="apple">Log in with Apple</div>
       <div class="separator">
         <span>or</span>
       </div>
+
       <div className="email">
         <input type="email" name="email" id="email" placeholder="Email" />
+        <svg
+            className="svgIcon"
+          xmlns="http://www.w3.org/2000/svg"
+          width="1em"
+          height="1em"
+          viewBox="0 0 256 256"
+        >
+          <path
+            fill="gray"
+            d="M247.31 124.76c-.35-.79-8.82-19.58-27.65-38.41C194.57 61.26 162.88 48 128 48S61.43 61.26 36.34 86.35C17.51 105.18 9 124 8.69 124.76a8 8 0 0 0 0 6.5c.35.79 8.82 19.57 27.65 38.4C61.43 194.74 93.12 208 128 208s66.57-13.26 91.66-38.34c18.83-18.83 27.3-37.61 27.65-38.4a8 8 0 0 0 0-6.5M128 192c-30.78 0-57.67-11.19-79.93-33.25A133.5 133.5 0 0 1 25 128a133.3 133.3 0 0 1 23.07-30.75C70.33 75.19 97.22 64 128 64s57.67 11.19 79.93 33.25A133.5 133.5 0 0 1 231.05 128c-7.21 13.46-38.62 64-103.05 64m0-112a48 48 0 1 0 48 48a48.05 48.05 0 0 0-48-48m0 80a32 32 0 1 1 32-32a32 32 0 0 1-32 32"
+          />
+        </svg>
       </div>
       <div className="password">
         <input
@@ -20,6 +48,18 @@ function LeftFrame() {
           id="password"
           placeholder="Password"
         />
+        <svg
+        className="svgIcon"
+          xmlns="http://www.w3.org/2000/svg"
+          width="1em"
+          height="1em"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="gray"
+            d="M6 22q-.825 0-1.412-.587T4 20V10q0-.825.588-1.412T6 8h1V6q0-2.075 1.463-3.537T12 1t3.538 1.463T17 6v2h1q.825 0 1.413.588T20 10v10q0 .825-.587 1.413T18 22zm0-2h12V10H6zm6-3q.825 0 1.413-.587T14 15t-.587-1.412T12 13t-1.412.588T10 15t.588 1.413T12 17M9 8h6V6q0-1.25-.875-2.125T12 3t-2.125.875T9 6zM6 20V10z"
+          />
+        </svg>
       </div>
       <div className="remember">
         <input type="checkbox" name="checkbox" id="checkbox" />
