1dcc5c89a3de8252bb55e20fc30504f3d2893ab6
Aditya Raj <138434890+Raj-Aditya-27@users.noreply.github.com>
2024-08-05 01:50:19 +0530

    image added

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import "../css/HomePage.css"
+import img1 from '../images/img1.jpg';
 
 function HomePage() {
   return (
     <div className='frame'>
         <div className='left'>Left</div>
-        <div className='right'>Right</div>
+        <div className='right'>
+            <img src={img1} alt="Happy girl with oranges" />
+        </div>
     </div>
   )
 }
